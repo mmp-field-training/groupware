01d81b2ec256b6fc2e147934c5d635826e18f1ee
thumbzzero <thumbzzero@gmail.com>
2022-12-27 13:09:18 +0900

    Add commute dto/controller

---
+++ src/main/java/com/mmp/groupware/web/commute/commuteDto.java
@@ -0,0 +1,18 @@
+package com.mmp.groupware.web.commute;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class commuteDto {
+}

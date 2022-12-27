5e93bdf4629386f5e551b2db4b0dba124823f1c0
thumbzzero <thumbzzero@gmail.com>
2022-12-27 11:48:05 +0900

    Add domain

--- src/main/java/com/mmp/groupware/domain/commute/commute.java
+++ src/main/java/com/mmp/groupware/domain/commute/commute.java
@@ -1,4 +1,39 @@
 package com.mmp.groupware.domain.commute;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="commute")
+@ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicUpdate
 public class commute {
+
+
+    @Column(nullable = true, length = 10)
+    private String atteYn;
+
+    @Column(nullable = true)
+    private LocalDateTime atteTime;
 }
+

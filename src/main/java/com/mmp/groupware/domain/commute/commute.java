38c31cc86d1981064122b1242c8e2563bee92e5c
eunsolH <eunsolhh@gmail.com>
2022-12-27 11:46:41 +0900

    Add domain

--- src/main/java/com/mmp/groupware/domain/commute/commute.java
+++ src/main/java/com/mmp/groupware/domain/commute/commute.java
@@ -1,4 +1,42 @@
 package com.mmp.groupware.domain.commute;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="auth")
+@ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicUpdate
+
 public class commute {
+
+    //출퇴근 여부 등록번호
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long atteNo;
+
+    //사원등록번호
+    @Column(nullable=false)
+    private Long stfNo;
+
 }

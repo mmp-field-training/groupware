dccd5fb438a6a5a6e4efc39deae0cf034e2689c4
thumbzzero <thumbzzero@gmail.com>
2022-12-28 09:24:41 +0900

    Update commute service

--- src/main/java/com/mmp/groupware/domain/commute/commuteService.java
+++ src/main/java/com/mmp/groupware/domain/commute/commuteService.java
@@ -4,13 +4,33 @@ import com.mmp.groupware.web.commute.commuteDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class commuteService {
     private final commuteMapper comMapper;
+
+    public List<commuteDto> getComList(Map<String, Object> search) {
+        return comMapper.getComList(search);
+    }
+
+    public List<commuteDto> getDailyComDetail(LocalDate date) {
+        return comMapper.getDailyComDetail(date);
+    }
+
     public List<commuteDto> getWeekComList(long atteNo) {
 
         return comMapper.getWeekComList(atteNo);
     }
+
+
+    // 출근 (등록)
+
+
+    // 퇴근 (등록)
+
+
+
 }

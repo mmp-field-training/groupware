901dbd3a20015ad775db8a6d43bf8b7194379578
parkjiyun <pjyun05200@gmail.com>
2022-12-28 09:29:04 +0900

    appr (보고 및 결재) dto 및 apprRefer entity, dto 추가

---
+++ src/main/java/com/mmp/groupware/web/appr/dto/apprDto.java
@@ -0,0 +1,43 @@
+package com.mmp.groupware.web.appr.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class apprDto {
+
+    private int rownum;
+
+    private Long apprNo;
+
+    private Long apprDivNo;
+
+    private Long apprStfNo;
+
+    private String apprTit;
+
+    private String apprCont;
+
+    private LocalDateTime apprDt;
+
+    private int apprState;
+
+    private String apprReject;
+
+    private LocalDate apprWrtDt;
+
+    private String apprWrtNm;
+
+    private LocalDateTime createDt;
+
+    private LocalDateTime updateDt;
+
+    private LocalDateTime deleteDt;
+
+}

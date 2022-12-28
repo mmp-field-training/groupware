97abc6cf258df1e0059a3fe6be8ca22cf3bd3223
eunsolH <eunsolhh@gmail.com>
2022-12-28 09:25:40 +0900

    add commuteservice

--- src/main/java/com/mmp/groupware/domain/commute/commuteService.java
+++ src/main/java/com/mmp/groupware/domain/commute/commuteService.java
@@ -4,6 +4,7 @@ import com.mmp.groupware.web.commute.commuteDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 @Service
 @RequiredArgsConstructor
@@ -13,4 +14,13 @@ public class commuteService {
 
         return comMapper.getWeekComList(atteNo);
     }
+
+    //월간 출퇴근 조회
+    public List<commuteDto> getMonthlyList(LocalDate date) {
+
+        return comMapper.getMonthlyList(date);
+    }
+
+
+
 }

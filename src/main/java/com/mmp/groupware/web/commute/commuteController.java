019a2cf3722ee4a946100ab026d196cf4ef4d2c2
eunsolH <eunsolhh@gmail.com>
2022-12-28 11:59:37 +0900

    add commuteController

--- src/main/java/com/mmp/groupware/web/commute/commuteController.java
+++ src/main/java/com/mmp/groupware/web/commute/commuteController.java
@@ -6,6 +6,9 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.mmp.groupware.domain.business.bsnRefer.bsnReferService;
+import com.mmp.groupware.domain.commute.commuteService;
+import com.mmp.groupware.web.staff.dto.staffDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,21 +32,63 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class commuteController {
 
+    private final sessionUtil sess;
+
+    private final commuteService comServ;
+
+    //출퇴근 등록
     @GetMapping("/add")
     public String add(HttpServletRequest request, Model model) {
 
         return "commute/add";
     }
 
+    //전체 출퇴근 조회
     @GetMapping("/list")
     public String list(HttpServletRequest request, Model model) {
 
         return "commute/list";
     }
 
+    //내 출근정보 조회
     @GetMapping("/myList")
-    public String myList(HttpServletRequest request, Model model) {
+    public String myList(HttpServletRequest request, Model model,long atte_no,
+                         @RequestParam(value="searchCode", defaultValue="1") int searchCode,
+                         @ModelAttribute pagingUtil pgn) {
+
+        // 세션 체크
+        if(sess.sessionCheck(request, "stfNo")) {
+            return "redirect:/staff/login";
+        }
+
+        // 상단메뉴
+        model.addAttribute("menu","record");
+
+        try {
+            Map<String, Object> search = new HashMap<String, Object>();
+            search.put("searchCode", searchCode);
+
+            /*  //페이징
+            pgn = new pagingUtil(cnt, pgn.getNowPg() , pgn.getCntPerPg());
+            search.put("start", pgn.getStart());
+            search.put("end", pgn.getCntPerPg());
+            model.addAttribute("cntPerPg" , pgn.getCntPerPg());
+             */
+
+            // 회원 목록 조회
+            List<commuteDto> commuteList = comServ. getPersonalComList(atte_no);
+
+            model.addAttribute("commuteRecord", commuteList);
+
+            // 완료 데이터 세팅 및 조회조건 바인드
+            //model.addAttribute("pgn", pgn);
+            model.addAttribute("search",search);
+
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
 
         return "commute/myList";
     }
+
 }

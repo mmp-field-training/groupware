1935dddcf7c51e07f9b6c85b7d512119bddebe1b
정다혜 <hyejjang120300@gmail.com>
2022-12-28 10:23:15 +0900

    add leaved function, addDto

---
+++ src/main/java/com/mmp/groupware/web/commute/commuteAddDto.java
@@ -0,0 +1,15 @@
+package com.mmp.groupware.web.commute;
+
+import lombok.*;
+
+import javax.websocket.Decoder;
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class commuteAddDto {
+
+
+}

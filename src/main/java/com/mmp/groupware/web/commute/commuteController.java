93d741a7cb249fd4d6dccd24cc597d1394b6550c
정다혜 <hyejjang120300@gmail.com>
2022-12-28 13:26:36 +0900

    add controller code

--- src/main/java/com/mmp/groupware/web/commute/commuteController.java
+++ src/main/java/com/mmp/groupware/web/commute/commuteController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.mmp.groupware.domain.commute.commuteService;
+import com.mmp.groupware.web.staff.dto.staffDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,18 +31,47 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class commuteController {
 
+    private final commuteService comServ;
+
+    //출퇴근하기
     @GetMapping("/add")
     public String add(HttpServletRequest request, Model model) {
 
         return "commute/add";
     }
 
+    //전체 출퇴근 현황
     @GetMapping("/list")
-    public String list(HttpServletRequest request, Model model) {
+    public String list(Model model, @ModelAttribute pagingUtil pgn,HttpServletRequest request) {
+//세션체크 일단 생략!
+        // 상단메뉴
+        model.addAttribute("menu","commute");
+        try {
 
+            // 회원 목록 조회
+            List<commuteDto> comList = comServ.getWeekComList(1);//임시값
+
+            model.addAttribute("comList", comList);
+
+            /*
+            // 결과 정보 개수 조회
+            long cnt = comList[0];
+
+            // 페이징
+            pgn = new pagingUtil(cnt, pgn.getNowPg() , pgn.getCntPerPg());
+            model.addAttribute("cntPerPg" , pgn.getCntPerPg());
+            */
+
+            // 완료 데이터 세팅 및 조회조건 바인드
+            model.addAttribute("pgn", pgn);
+
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
         return "commute/list";
     }
 
+    //나의 출퇴근현황
     @GetMapping("/myList")
     public String myList(HttpServletRequest request, Model model) {
 
